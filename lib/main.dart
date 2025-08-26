6cca463f8ebf861b00e24a407b8a080e143fb34f
fahmiorim <zulfahmi.mail@gmail.com>
2025-08-26 17:45:57 +0700

    Require external API URL configuration

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import 'config/app_theme.dart';
 import 'package:just_audio_background/just_audio_background.dart';
 import 'config/app_routes.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
   await initializeDateFormatting('id_ID', null);
   await JustAudioBackground.init(
     androidNotificationChannelId: 'com.ryanheise.bg_demo.channel.audio',
