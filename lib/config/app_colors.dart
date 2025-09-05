c188aae204027b3e58f7ba2ab6bc5ea6e03bc8bc
fahmiorim <zulfahmi.mail@gmail.com>
2025-09-06 06:46:19 +0700

    refactor(theme): use AppColors in AppTheme

--- lib/config/app_colors.dart
+++ lib/config/app_colors.dart
@@ -90,8 +90,12 @@ class AppColors {
   static const Color redAccent = Colors.redAccent;
   static const Color amber300 = Color(0xFFFFD54F);
   static const Color orange700 = Color(0xFFF57C00);
+  static const Color blue800 = Color(0xFF1565C0);
+  static const Color blueGrey800 = Color(0xFF37474F);
   static const Color grey100 = Color(0xFFF5F5F5);
+  static const Color grey200 = Color(0xFFEEEEEE);
   static const Color grey300 = Color(0xFFE0E0E0);
+  static const Color grey400 = Color(0xFFBDBDBD);
   static const Color grey600 = Color(0xFF757575);
   static const Color grey700 = Color(0xFF616161);
   static const Color grey800 = Color(0xFF424242);
